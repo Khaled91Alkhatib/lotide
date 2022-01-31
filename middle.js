2495552896082802118a0d82509e09df2069b196
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-01-31 15:00:18 -0500

    Middle task

---
+++ middle.js
@@ -0,0 +1,32 @@
+function eqArrays(array1, array2) {
+  for(let i = 0; i < array1.length; i++){
+    if(array1[i] !== array2[i]){
+      return false
+    }
+  } return true;
+ }
+
+function assertArraysEqual(array1, array2) {
+  if(eqArrays(array1, array2)){
+   console.log(`✅✅✅ Assertion passed: ${array1} === ${array2}`);
+  } else{
+   console.log(`🛑🛑🛑 Assertion failed: ${array1} !== ${array2}`);
+  }
+}
+
+function middle(newArray) {
+  if(newArray.length === 1 || newArray.length === 2) {
+    return [];
+  }else if(newArray.length % 2 === 0) {
+    return [newArray[newArray.length / 2 - 1], newArray[newArray.length/2]];
+  }else {
+    return [newArray[Math.floor(newArray.length / 2)]];
+  }
+}
+
+console.log(middle([1])) // => []
+console.log(middle([1, 2])) // => []
+console.log(middle([1, 2, 3])) // => [2]
+console.log(middle([1, 2, 3, 4, 5])) // => [3]
+console.log(middle([1, 2, 3, 4])) // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
