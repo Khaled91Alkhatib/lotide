fd6d85d69ab558b305c4510e4695d716205ed5f8
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-02-01 18:56:04 -0500

    letterPositions task

---
+++ letterPositions.js
@@ -0,0 +1,31 @@
+function eqArrays(array1, array2) {
+  for(let i = 0; i < array1.length; i++) {
+    if(array1[i] !== array2[i]){
+      return false;
+    }
+  } return true;
+ }
+
+ function assertArraysEqual(array1, array2) {
+   if(eqArrays(array1, array2)){
+    console.log(`✅✅✅ Assertion passed: ${array1} === ${array2}`);
+   } else{
+    console.log(`🛑🛑🛑 Assertion failed: ${array1} !== ${array2}`);
+   }
+ }
+
+const letterPositions = function(sentence) {
+  let results = {};
+  for(let i = 0; i < sentence.length; i++) {
+    let char = sentence[i];
+    if(results[char]) {
+      results[char].push(i);
+    } else {
+      results[char] = [i];
+    }
+  }
+  return results;
+};
+assertArraysEqual(letterPositions("lighthouselabs").l, [0, 10]);
+
+// console.log(letterPositions("lighthouselabs"))
