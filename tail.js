dc1550858349e962de2c7e3d7e1c1eedc7a5c93d
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-02-14 19:39:33 -0500

    Refactor tail.js and minor cleanup in head and test files

--- tail.js
+++ tail.js
@@ -1,21 +1,8 @@
-let assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require("./assertEqual");
 
 let tail = function(array) {
   return array.slice(1);
 };
-tail([1, 2, 3]);
+// tail([1, 2, 3]);
 
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-// assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
-assertEqual(result[0], 'Lighthouse');
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
