456c41eb2817b1e1b1d722da8c33c7eef6767949
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-02-14 19:57:11 -0500

    Refactoring assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,10 +1,4 @@
-function eqArrays(array1, array2) {
-  for(let i = 0; i < array1.length; i++) {
-    if(array1[i] !== array2[i]){
-      return false;
-    }
-  } return true;
- }
+const eqArrays = require("./eqArrays");
 
  function assertArraysEqual(array1, array2) {
    if(eqArrays(array1, array2)){
@@ -14,4 +8,4 @@ function eqArrays(array1, array2) {
    }
  }
 
- console.log(assertArraysEqual([1, "2", 3], [1, 2, 3]));
+ module.exports = assertArraysEqual;
